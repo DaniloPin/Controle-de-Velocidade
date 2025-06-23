d7360321fa2f1c128315b349ce51a96f8456dea2
Danilo Gonçalves Pinheiro <124277720+DaniloPin@users.noreply.github.com>
2025-06-23 19:46:47 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Veiculo veiculo = new Veiculo("RJ-310823");
+veiculo.AtualizarVelocidade(72.5);
+
+veiculo.ExibirSaida();
---
+++ Veiculo.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.Metrics;
+using System.Reflection.Metadata.Ecma335;
+
+public class Veiculo 
+{
+    public string Placa {  get; set; }
+    private double velocidadeAtual;
+
+    public Veiculo(string placa) {
+        Placa = placa;
+    }
+
+    public void AtualizarVelocidade (double novaVelocidade) {
+
+        velocidadeAtual = novaVelocidade;
+    }
+
+    public double VelocidadeAtual {
+        get {
+            return velocidadeAtual;
+        }
+    }
+
+    public void ExibirSaida() {
+
+        Console.WriteLine($"Veiculo: {Placa}");
+        Console.WriteLine($"Velocidade: {velocidadeAtual} KM/H");
+    }
+}
